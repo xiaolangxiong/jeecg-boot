7d980ed6a6e550b7b6f50126c8144d751dabe799
笔记本 <13719343938@163.com>
2022-03-28 00:33:23 +0800

    提交代码

--- jeecg-boot/jeecg-boot-module-business/src/main/java/org/jeecg/modules/controller/LoginByMailController.java
+++ jeecg-boot/jeecg-boot-module-business/src/main/java/org/jeecg/modules/controller/LoginByMailController.java
@@ -1,6 +1,6 @@
-package com.boxiu.login.controller;
+package org.jeecg.modules.controller;
 
-import com.boxiu.login.service.MailService;
+import org.jeecg.modules.service.MailService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
--- jeecg-boot/jeecg-boot-module-business/src/main/java/org/jeecg/modules/service/MailService.java
+++ jeecg-boot/jeecg-boot-module-business/src/main/java/org/jeecg/modules/service/MailService.java
@@ -1,11 +1,11 @@
-package com.boxiu.login.service;
+package org.jeecg.modules.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class MailService {
     @Autowired
     JavaMailSender javaMailSender;
